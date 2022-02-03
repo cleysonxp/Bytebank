2f22cc399fc12b27338c801d0405b61ee7321b65
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2022-02-03 08:35:19 -0300

    App ByteBank

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          secondary: Colors.blueAccent
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
